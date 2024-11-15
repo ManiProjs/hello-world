c2b1a67626853a2cdc88456ab7b8d8fc4116fe51
Mani Arasteh <mani.arasteh92@gmail.com>
2024-11-15 19:03:47 +0330

    Initial commit🚀

---
+++ rust/src/main.rs
@@ -0,0 +1,4 @@
+// Cargo generated code itself, I didn't do anything
+fn main() {
+    println!("Hello, world!");
+}
---
+++ rust/target/debug/deps/rust-f80b2eb40aecbfec.d
@@ -0,0 +1,5 @@
+C:\Users\Asus\Documents\GitHub\hello-world\rust\target\debug\deps\librust-f80b2eb40aecbfec.rmeta: src/main.rs
+
+C:\Users\Asus\Documents\GitHub\hello-world\rust\target\debug\deps\rust-f80b2eb40aecbfec.d: src/main.rs
+
+src/main.rs:
